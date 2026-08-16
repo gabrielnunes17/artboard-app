d5fed515b82c3d5b1736281be208fc7184536e7d
gabrielnunes17 <contact.gabrielnsouza@gmail.com>
2026-08-16 11:08:16 -0300

    feat: implementa input de escolha de cor

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 const btnCor = document.getElementById("btn-cor");
+const inputCor = document.getElementById("input-cor");
 const board = document.getElementById("board");
-
-function gerarCorAleatoria() {
-  const caracteres = "0123456789ABCDEF";
-  let cor = "#";
-  for (let i = 0; i < 6; i++) {
-    cor += caracteres[Math.floor(Math.random() * 16)];
-  }
-  return cor;
-}
 
 function adicionarQuadradoColorido() {
   const novoQuadrado = document.createElement("div");
   novoQuadrado.classList.add("artboard-item");
 
-  const corGerada = gerarCorAleatoria();
-  novoQuadrado.style.backgroundColor = corGerada;
+  novoQuadrado.style.backgroundColor = inputCor.value;
 
   novoQuadrado.addEventListener("click", function () {
     novoQuadrado.remove();
